b26c6bed3aae89ad56ca4a76b406128883dea59a
Shanjiv <shanjiv@hotmail.com>
2024-10-02 09:40:32 +0200

    add withdraw

--- Fundme.sol
+++ Fundme.sol
@@ -14,16 +14,36 @@ contract FundMe {
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable{
         require(msg.value.getConversionRate() >= minimumUsd, "didnt send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
         for(uint256 funderIndex = 0;  funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+        funders = new address[](0);
+/*         //transfer
+        payable(msg.sender).transfer(address(this).balance);
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess,"Send failed");
+        //call */
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 }
